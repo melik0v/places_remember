084705d4651d9be853f3eefcf9ed1f1727daeeb3
melik0v <rmelik00v@gmail.com>
2022-07-23 19:55:33 +0700

    Fixed pipeline 'get_avatar'

--- places_remember/places_remember/settings.py
+++ places_remember/places_remember/settings.py
@@ -140,9 +140,18 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 SOCIAL_AUTH_VK_OAUTH2_KEY = "8184001"
 SOCIAL_AUTH_VK_OAUTH2_SECRET = "WfvytZ9nXRVJbXswDMG1"
 
-# SOCIAL_AUTH_PIPELINE = (
-#     'user_profile.pipeline.get_avatar'
-# )
+SOCIAL_AUTH_PIPELINE = (
+    "social_core.pipeline.social_auth.social_details",
+    "social_core.pipeline.social_auth.social_uid",
+    "social_core.pipeline.social_auth.auth_allowed",
+    "social_core.pipeline.social_auth.social_user",
+    "social_core.pipeline.user.get_username",
+    "social_core.pipeline.user.create_user",
+    "social_core.pipeline.social_auth.associate_user",
+    "social_core.pipeline.social_auth.load_extra_data",
+    "social_core.pipeline.user.user_details",
+    "user_profile.pipeline.get_avatar",
+)
 
 LOGIN_REDIRECT_URL = "/"
 SOCIAL_AUTH_VK_OAUTH2_SCOPE = ["email", "photos"]
--- places_remember/user_profile/pipeline.py
+++ places_remember/user_profile/pipeline.py
@@ -3,10 +3,9 @@ def get_avatar(backend, response, user=None, *args, **kwargs):
     url = None
 
     if backend.name == "vk-oauth2":
-        print(user.avatar)
         url = response.get("photo", "")
-        print(user.avatar)
-
     if url:
         user.avatar = url
         user.save()
+        print("hello")
+        print(user.avatar)
